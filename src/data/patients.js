d8762ad22585f739910c98a4bb701d8d67eae3aa
cosuoji <qelvinz@gmail.com>
2025-08-26 22:20:14 +0100

    Update patients.js

--- src/data/patients.js
+++ src/data/patients.js
@@ -1,27 +1,7 @@
 export const patients = [
+
   {
-    patientName: "John Smith",
-    roomNumber: "201A",
-    diagnoses: "Pneumonia",
-    dateCheckedIn: "2025-08-20",
-    possibleDischargeDate: "2025-08-28"
-  },
-  {
-    patientName: "Amaka Uzo",
-    roomNumber: "105B",
-    diagnoses: "Fractured Radius",
-    dateCheckedIn: "2025-08-22",
-    possibleDischargeDate: "2025-08-30"
-  },
-  {
-    patientName: "Ibrahim Bello",
-    roomNumber: "309",
-    diagnoses: "Hypertension",
-    dateCheckedIn: "2025-08-18",
-    possibleDischargeDate: ""
-  },
-  {
-    patientName: "Chioma N.",
+    patientName: "Michael Belfield",
     roomNumber: "410C",
     diagnoses: "Migraine",
     dateCheckedIn: "2025-08-24",
